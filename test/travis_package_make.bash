38cc266530bfc2450655b475dfa536fb23fdad97
Hideki Ogura <b1415988@planet.kanazawa-it.ac.jp>
2017-12-13 16:27:57 +0000

    Changed files

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -3,8 +3,9 @@
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
+#clone pimouse_ros
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/Hideki34/pimouse_ros.git
+git clone https://github.com/Hideki34/pimouse_ros.git
 
 cd ~/catkin_ws
 catkin_make
